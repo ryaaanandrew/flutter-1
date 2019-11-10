45e578bb424aae3a284735b6666bf1d5253a32da
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-11-09 21:02:23 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Hello World'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Hello Ninjas'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Herro World'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(90.00),
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
